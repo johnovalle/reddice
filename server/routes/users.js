960f13a9103914658d8abada1b53f2d39204529f
John Ovalle <johnovalle@gmail.com>
2018-04-26 08:56:37 -0400

    save user in database

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import validateInput from '../shared/validations/singup';
+import bcrypt from 'bcrypt';
+
+import User from '../models/user';
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
   const {errors, isValid } = validateInput(req.body);
-  console.log(req.body, errors, isValid);
   if (isValid) {
-    res.json({ success: true});
+    const { username, email, password, timezone } = req.body;
+    const password_digest = bcrypt.hashSync(password, 10);
+
+    User.forge({ 
+      username, email, timezone, password_digest
+    }, { hasTimestamps: true } ).save()
+      .then(user => res.json({ success: true}))
+      .catch(error => res.status(500).json({error}));
   } else {
     res.status(400).json(errors);
   }
